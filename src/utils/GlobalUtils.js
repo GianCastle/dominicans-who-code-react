a0ad739961745214774b63d0e2e5e68d94225812
Giancarlos <gc.c@outlook.com>
2019-09-15 17:51:21 -0400

    improves: renames src/utils to src/GlobalUtils

--- src/utils/GlobalUtils.js
+++ src/utils/GlobalUtils.js
@@ -1,17 +1,16 @@
 
 /**
  * TODO: make an stronger regex
- * 
+ * @func
  * @param {string} word 
  * @returns {number} -1 is the string is empty, 0 if the string is not a URL that starts
  *  with http|https and 1 is is valid URL
  */
 
-export const checkUrl = word => {
-    return (!word) ? -1
+export const checkUrl = word => 
+     (!word) ? -1
         : (/^(http|https|ftp)/.test(word)) ? 1
             : 0
-}
 
 /**
  * Check if the object is empty
@@ -23,9 +22,9 @@ export const isEmptyObject = obj =>
 
 /**
  * Search for a property inside an object.
- * 
- * @param {string} dflt the value we set if the object property is undefined or null
- * @param {*} c the property we're searching for
+ * @func
+ * @param {String} defaultValue the value we set if the object property is undefined or null
+ * @param {String} criteria the property we're searching for
  * @param {*} obj the object where we want to search 
  * 
  * @returns {defaultValue | obj[criteria]} the default value we passed or the property value we're searching for.
@@ -33,8 +32,20 @@ export const isEmptyObject = obj =>
 export const pathOr = (defaultValue = '', criteria = '', obj) =>
     obj[criteria] || defaultValue
 
-export const randomize = () =>
-    0.5 - Math.random()
+/**
+ * Gets a Random between a range as argument
+ * @func 
+ * @param {Number} range the range of the random generator 
+ * @returns {Number} num a random number
+ */
+export const randomize = (range = 0.5) =>
+    range - Math.random()
 
+/**
+ * Shuffles an Iterable Object
+ * @func
+ * @param {Array} data the array that we want to shuffle
+ * @returns {Array} data sorted in a random way  
+ */
 export const shuffle = data =>
     data.sort(randomize)
